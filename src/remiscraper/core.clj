60dd782cd51992831a90371fe2ee6b4c2996690c
vimtaku <vimtaku@gmail.com>
2014-10-04 12:55:29 +0900

    Fix to use chefs

--- src/remiscraper/core.clj
+++ src/remiscraper/core.clj
@@ -17,6 +17,7 @@
 
 (def remi "https://ryorisapuri.jp")
 (def ingredients-page (str remi "/search/ingredients"))
+(def chefs-page (str remi "/chefs"))
 
 (defn get-categories-uris []
   (let [ingredients-html (e/html-resource (reader ingredients-page))]
@@ -30,11 +31,24 @@
   )
 )
 
-(defn add-offset [url offset]
-  (str url "&offset=" offset)
+(defn get-chefs-uris []
+  (let [chefs-html (e/html-resource (reader chefs-page))]
+    (map #(str remi %)
+      (filter (fn [u] (re-find #"chefs/" u))
+        (map #(get-in % [:attrs :href]) (e/select chefs-html [:a]))
+      )
+    )
+  )
 )
 
-(defn get-category-json [url]
+(defn add-offset [url offset]
+  (if (re-find #"\?" url)
+    (str url "&offset=" offset)
+    (str url "?offset=" offset)
+  )
+)
+
+(defn get-json [url]
   (client/get url
     {:headers
      {:X-Requested-With "XMLHttpRequest"
@@ -44,17 +58,20 @@
       :User-Agent "remiscraper"
      }})
 )
-(defn get-category-json-with-sleep [url ms]
+(defn get-json-with-sleep [url ms]
   (Thread/sleep ms)
-  (get-category-json url)
+  (get-json url)
 )
 (defn res-logger [res]
   (write-result res "res.log")
 )
 
-(defn get-category-recipe-title [c-url offset]
+(defn sleeptime []
+  (+ 2000 (rand-int 3000))
+)
+(defn get-recipe-title [c-url offset]
   (let [
-         c-json (get-category-json-with-sleep (add-offset c-url offset) 3000)
+         c-json (get-json-with-sleep (add-offset c-url offset) (sleeptime))
          body-json (json/read-str (:body c-json))
          has_next? (get-in body-json ["count_info" "has_next"])
          next-offset (get-in body-json ["count_info" "offset"])
@@ -64,7 +81,7 @@
        ;(res-logger (str c-json))
 
        (if has_next?
-         (concat (result) (get-category-recipe-title c-url next-offset))
+         (concat (result) (get-recipe-title c-url next-offset))
          (result)
        )
   )
@@ -74,15 +91,16 @@
 (defn scrape-all-recipe-title [dest]
   (defn scrape-and-write [u]
     (write-result
-      (str (clojure.string/join "\n" (get-category-recipe-title u 0)) "\n")
+      (str (clojure.string/join "\n" (get-recipe-title u 0)) "\n")
     dest)
   )
 
-  (doall (map scrape-and-write (get-categories-uris)))
+  (doall (map scrape-and-write (get-chefs-uris)))
 )
+;(scrape-all-recipe-title "/tmp/result.txt")
 
 
 (defn -main [& args]
-  (scrape-all-recipe-title "/tmp/result.txt")
+  (doall (map println (get-recipe-title (first (get-chefs-uris)) 0) ))
 )
 
